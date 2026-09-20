c30f02ce64aad0f04dc21d600ce5d7ebf14e57c8
Ailton Silva <personalizadoscanecas47@gmail.com>
2026-09-20 18:53:25 -0300

    Adiciona menu responsivo em telas pequenas

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+  // configuração do seu aplicativo Firebase
+    const firebaseConfig ={
+  apiKey: "AIzaSyBbAeVNgz1LGrkAgHEwNttr9h3JlSeYG2o",
+  authDomain: "brindes-arte.firebaseapp.com",
+  projectId: "brindes-arte",
+  storageBucket: "brindes-arte.firebasestorage.app",
+  messagingSenderId: "699924584237",
+  appId: "1:699924584237:web:ffa81a6ce2717a6558a5a4"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
